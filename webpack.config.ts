fa163f36a16ea532bc18083e9cf384f17776f98f
Kasper Emil Storgaard <kasper.storgaard@falcon.io>
2016-11-12 18:55:09 +0100

    refactor(webpack.config): simplify

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,30 +1,14 @@
 /// <reference path="./node_modules/@types/node/index.d.ts" />
-/**
- * To learn more about how to use Easy Webpack
- * Take a look at the README here: https://github.com/easy-webpack/core
- **/
 import { generateConfig, get, stripMetadata, EasyWebpackConfig } from '@easy-webpack/core';
 import * as path from 'path';
-
-import * as envProd from '@easy-webpack/config-env-production';
-import * as envDev from '@easy-webpack/config-env-development';
+import * as AureliaWebpackPlugin from 'aurelia-webpack-plugin';
 import * as aurelia from '@easy-webpack/config-aurelia';
 import * as typescript from '@easy-webpack/config-typescript';
 import * as html from '@easy-webpack/config-html';
-import * as css from '@easy-webpack/config-css';
-import * as fontAndImages from '@easy-webpack/config-fonts-and-images';
-import * as globalBluebird from '@easy-webpack/config-global-bluebird';
-import * as globalJquery from '@easy-webpack/config-global-jquery';
 import * as generateIndexHtml from '@easy-webpack/config-generate-index-html';
 import * as commonChunksOptimize from '@easy-webpack/config-common-chunks-simple';
-import * as copyFiles from '@easy-webpack/config-copy-files';
-import * as uglify from '@easy-webpack/config-uglify';
-import * as generateCoverage from '@easy-webpack/config-test-coverage-istanbul';
 
-const ENV: 'development' | 'production' | 'test' = process.env.NODE_ENV && process.env.NODE_ENV.toLowerCase() || (process.env.NODE_ENV = 'development');
-
-// basic configuration:
-const title = 'Aurelia Navigation Skeleton';
+const title = 'Aurelia sample';
 const baseUrl = '/';
 const rootDir = path.resolve();
 const srcDir = path.resolve('src');
@@ -35,12 +19,10 @@ const coreBundles = {
     'aurelia-bootstrapper-webpack',
     'aurelia-polyfills',
     'aurelia-pal',
-    'aurelia-pal-browser',
-    'bluebird'
+    'aurelia-pal-browser'
   ],
   // these will be included in the 'aurelia' bundle (except for the above bootstrap packages)
   aurelia: [
-    'aurelia-bootstrapper-webpack',
     'aurelia-binding',
     'aurelia-dependency-injection',
     'aurelia-event-aggregator',
@@ -52,10 +34,7 @@ const coreBundles = {
     'aurelia-logging',
     'aurelia-logging-console',
     'aurelia-metadata',
-    'aurelia-pal',
-    'aurelia-pal-browser',
     'aurelia-path',
-    'aurelia-polyfills',
     'aurelia-route-recognizer',
     'aurelia-router',
     'aurelia-task-queue',
@@ -66,52 +45,63 @@ const coreBundles = {
   ]
 }
 
-/**
- * Main Webpack Configuration
- */
-let config = generateConfig(
-  {
-    entry: {
-      'app': ['./src/main' /* this is filled by the aurelia-webpack-plugin */],
-      'aurelia-bootstrap': coreBundles.bootstrap,
-      'aurelia': coreBundles.aurelia.filter(pkg => coreBundles.bootstrap.indexOf(pkg) === -1)
-    },
-    output: {
-      path: outDir,
-    },
+const mainConfig = {
+  devtool: 'inline-source-map',
+  entry: {
+    app: ['./src/main'],
+    'aurelia-bootstrap': coreBundles.bootstrap,
+    aurelia: coreBundles.aurelia
   },
+  metadata: {
+    root: rootDir,
+    src: srcDir,
+    title,
+    baseUrl
+  },
+  output: {
+    path: outDir,
+    filename: '[name].bundle.js',
+    sourceMapFilename: '[name].bundle.map',
+    chunkFilename: '[id].chunk.js'
+  },
+  devServer: {
+    port: 9000,
+    historyApiFallback: true,
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000
+    },
+    outputPath: outDir
+  },
+  plugins: [
+    new AureliaWebpackPlugin({
+      root: rootDir,
+      src: srcDir,
+      title,
+      baseUrl
+    })
+  ],
+  resolve: {
+    modules: [srcDir].concat(get(this, 'resolve.modules', ['node_modules']))
+  }
+};
 
-  /**
-   * Don't be afraid, you can put bits of standard Webpack configuration here
-   * (or at the end, after the last parameter, so it won't get overwritten by the presets)
-   * Because that's all easy-webpack configs are - snippets of premade, maintained configuration parts!
-   * 
-   * For Webpack docs, see: https://webpack.js.org/configuration/
-   */
+const typescriptConfig = typescript({});
 
-  ENV === 'test' || ENV === 'development' ? 
-    envDev(ENV !== 'test' ? {} : {devtool: 'inline-source-map'}) :
-    envProd({ /* devtool: '...' */ }),
+const chunkConfig = commonChunksOptimize({
+  appChunkName: 'app',
+  firstChunk: 'aurelia-bootstrap'
+});
 
-  aurelia({root: rootDir, src: srcDir, title: title, baseUrl: baseUrl}),
-  typescript(ENV !== 'test' ? {} : { options: { doTypeCheck: false, sourceMap: false, inlineSourceMap: true, inlineSources: true } }),
-  html(),
-  css({ filename: 'styles.css', allChunks: true, sourceMap: false }),
-  fontAndImages(),
-  globalBluebird(),
-  globalJquery(),
-  generateIndexHtml({minify: ENV === 'production'}),
+const htmlConfig = html();
+const indexConfig = generateIndexHtml()
 
-  ...(ENV === 'production' || ENV === 'development' ? [
-      commonChunksOptimize({appChunkName: 'app', firstChunk: 'aurelia-bootstrap'}),
-      copyFiles({patterns: [{ from: 'favicon.ico', to: 'favicon.ico' }]})
-    ] : [
-    /* ENV === 'test' */
-    generateCoverage({ options: { esModules: true } })
-  ]),
-
-  ENV === 'production' ?
-    uglify({debug: false, mangle: { except: ['cb', '__webpack_require__'] }}) : {}
+const config = generateConfig(
+  mainConfig,
+  typescriptConfig,
+  htmlConfig,
+  indexConfig,
+  chunkConfig
 );
 
 module.exports = stripMetadata(config);
