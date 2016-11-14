61fc2a5c337555035eb43507a132873b226b24b4
Kasper Emil Storgaard <kasper.storgaard@falcon.io>
2016-11-14 09:46:37 +0100

    test(app): initial tests

---
+++ src/app.spec.ts
@@ -0,0 +1,48 @@
+#!/usr/bin/env ts-node
+const { test } = require('tape');
+const td = require('testdouble');
+
+import { App } from './app';
+
+function setup () {
+  const config = td.object(['map', 'configure']);
+  const router = {};
+
+  return {
+    config,
+    router,
+    sut: new App()
+  };
+}
+
+// function tearDown () {}
+
+test('app.configureRouter(): router', (t) => {
+  const { sut, router, config } = setup();
+
+  sut.configureRouter(config, router);
+  t.ok(sut.router,
+    '.configureRouter() should set the "router" property');
+
+  t.end();
+});
+
+test('app.configureRouter(): title', (t) => {
+  const { sut, router, config } = setup();
+
+  const routeConfig = {
+    moduleId: './splash',
+    name: 'splash',
+    nav: true,
+    route: ['', 'splash'],
+    title: 'Splash'
+  };
+
+  sut.configureRouter(config, router);
+
+  t.doesNotThrow(() => {
+    td.verify(config.map([routeConfig]));
+  }, '.configureRouter(), should set the .title property to "Aurelia"');
+
+  t.end();
+});
